80cd598f70d476b00c3f3486cefd42db7531e2cf
itsashishg <itsashishg@duck.com>
2023-12-11 00:07:41 +0530

    changes done.

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -35,7 +35,6 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   noStore();
-  await new Promise((resolve) => setTimeout(resolve, 3000));
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
